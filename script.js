8b437077199f2c9cebeff0b98b2d596cedd27b9d
elisatimmerman <45420467+elisatimmerman@users.noreply.github.com>
2019-02-26 20:40:18 +0100

    JS filteren DEF

--- script.js
+++ script.js
@@ -2,20 +2,40 @@
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
 
+
 var body = document.querySelector('body');
 
-var blauwKnop = document.querySelector('form input[name=blauw]');
+var blauwKnop = document.querySelector('form input[name=blue]');
 
-var bruinKnop = document.querySelector('form input[name=bruin]');
+var bruinKnop = document.querySelector('form input[name=brown]');
+
+var groenKnop = document.querySelector('form input[name=green]');
+
+var roodKnop = document.querySelector('form input[name=red]');
 
 
 
+//Met deze functie zorg je ervoor dat de kleuren bruin, groen en rood worden weggehaald. Dit heb je in css geregeld.//
 blauwKnop.onclick = function () {
-    body.classList.toggle('blauw');
+    body.classList.toggle('brown');
+    body.classList.toggle('green');
+    body.classList.toggle('red');
 };
-
 
 bruinKnop.onclick = function () {
-    body.classList.toggle('bruin');
+    body.classList.toggle('blue');
+    body.classList.toggle('green');
+    body.classList.toggle('red');
 };
 
+groenKnop.onclick = function () {
+    body.classList.toggle('blue');
+    body.classList.toggle('brown');
+    body.classList.toggle('red');
+};
+
+roodKnop.onclick = function () {
+   body.classList.toggle('blue');
+    body.classList.toggle('green');
+    body.classList.toggle('brown');
+};
